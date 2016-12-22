706908a8b8920775d02858a2b1cb1c0b4ad1f2c3
Thomuse <thomas_stemp@hotmail.com>
2016-12-22 15:40:41 +0100

    okkkk

--- GestionBibliothèque/DAL/Connection.cs
+++ GestionBibliothèque/DAL/Connection.cs
@@ -9,5 +9,5 @@ namespace DAL
     public static class Connection
     {
         public static string _DefaultConnectionString = "Data Source=MICHAEL;Initial Catalog=Bibliotheque;User ID=sa;Password=justloco";
-    }
+        public static string _DefaultConnectionString = "Data Source=(local);Initial Catalog=Bibliotheque;Persist Security Info=True;User ID=sa";
 }
